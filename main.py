8bead06c61a1f34ef80c51f55361529a5596d490
Eric Brechner <ericbrec@msn.com>
2021-12-05 16:26:35 -0800

    Add listbox and associated scollbar.

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+import numpy as np
 import tkinter as tk
 from OpenGL.GL import *
 from pyopengltk import OpenGLFrame
 
-class frame(OpenGLFrame):
+class Spline:
+    def __init__(self, point):
+        self.point = np.array(point)
+
+    def __str__(self):
+        return "[{0}, {1}, z: {2}]".format(self.point[0], self.point[1], self.point[2])
+
+class SplineOpenGLFrame(OpenGLFrame):
 
     def initgl(self):
         glViewport(0, 0, self.width, self.height)
@@ -33,9 +41,26 @@ class frame(OpenGLFrame):
         glEnd()
         glFlush()
 
+def ListChanged(event):
+    splineList = event.widget
+    spline = splineList.get(splineList.curselection())
+    print(spline)
+
 if __name__=='__main__':
 
     root = tk.Tk()
-    app = frame(root, width=500, height=500)
-    app.pack(fill=tk.BOTH, expand=tk.YES)
-    app.mainloop()
+    root.title('PyNub')
+    splineList = tk.Listbox(root, height=20)
+    splineList.grid(row=0, column=0, sticky=tk.NS)
+    splineList.bind('<<ListboxSelect>>', ListChanged)
+    splineList.insert(tk.END, Spline([1,2,3]))
+    splineList.insert(tk.END, Spline([-1,-2,-3]))
+    for i in range(30):
+        splineList.insert(tk.END, Spline([i,0,-1]))
+    verticalScroll = tk.Scrollbar(root, orient=tk.VERTICAL)
+    verticalScroll.grid(row=0, column=1, sticky=tk.NS)
+    splineList.configure(yscrollcommand=verticalScroll.set)
+    verticalScroll.config(command=splineList.yview)
+    frame = SplineOpenGLFrame(root, width=500, height=500)
+    frame.grid(row=0, column=2)
+    root.mainloop()
