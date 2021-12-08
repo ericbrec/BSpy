2e144f6b7e80b75111785d04f850b5be305aba5a
Eric Brechner <ericbrec@msn.com>
2021-12-07 22:23:50 -0800

    Got spline listbox and drawing working. Tons of cleanup.

--- main.py
+++ main.py
@@ -8,59 +8,73 @@ class Spline:
         self.point = np.array(point)
 
     def __str__(self):
-        return "[{0}, {1}, z: {2}]".format(self.point[0], self.point[1], self.point[2])
+        return "[{0}, {1}]".format(self.point[0], self.point[1])
+    
+    def Draw(self):
+        glColor3f(1.0, 0.0, 0.0)
+        glBegin(GL_LINE_STRIP)
+        for point in self.point:
+            glVertex2f(point[0], point[1])
+        glEnd()
 
 class SplineOpenGLFrame(OpenGLFrame):
 
+    def __init__(self, *args, **kw):
+        OpenGLFrame.__init__(self, *args, **kw)
+        self.splineDrawList = []
+
     def initgl(self):
         glViewport(0, 0, self.width, self.height)
-        glClearColor(0.0,1.0,0.0,0.0)
+        glClearColor(0.0, 0.0, 0.0, 0.0)
 
-        # setup projection matrix
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        glOrtho(0, self.width, self.height, 0, -1, 1)
+        glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0, 1.0)
 
-        # setup identity model view matrix
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-
         
     def redraw(self):
 
         glClear(GL_COLOR_BUFFER_BIT)
-
         glLoadIdentity()
 
-        glBegin(GL_LINES)
-        glColor3f(1.0,0.0,3.0)
-        glVertex2f(200,100)
-        glVertex2f(100,100)
-        glVertex2f(100,100)
-        glVertex2f(200,200)
-        glEnd()
+        for spline in self.splineDrawList:
+            spline.Draw()
+
         glFlush()
 
-def ListChanged(event):
-    splineList = event.widget
-    spline = splineList.get(splineList.curselection())
-    print(spline)
+class PyNubApp(tk.Tk):
+    def __init__(self, *args, **kw):
+        tk.Tk.__init__(self, *args, **kw)
+        self.title('PyNub')
+
+        self.listBox = tk.Listbox(self, selectmode=tk.MULTIPLE)
+        self.listBox.pack(side=tk.LEFT, fill=tk.Y)
+        self.listBox.bind('<<ListboxSelect>>', self.ListChanged)
+
+        self.verticalScroll = tk.Scrollbar(self, orient=tk.VERTICAL)
+        self.verticalScroll.pack(side=tk.LEFT, fill=tk.Y)
+        self.listBox.configure(yscrollcommand=self.verticalScroll.set)
+        self.verticalScroll.config(command=self.listBox.yview)
+
+        self.frame = SplineOpenGLFrame(self, width=500, height=500)
+        self.frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=tk.YES)
+
+        self.splineList = []
+
+    def AddSpline(self, spline):
+        self.splineList.append(spline)
+        self.listBox.insert(tk.END, spline)
+
+    def ListChanged(self, event):
+        self.frame.splineDrawList = []
+        for item in self.listBox.curselection():
+            self.frame.splineDrawList.append(self.splineList[item])
+        self.frame.tkExpose(None)
 
 if __name__=='__main__':
-
-    root = tk.Tk()
-    root.title('PyNub')
-    splineList = tk.Listbox(root, height=20)
-    splineList.grid(row=0, column=0, sticky=tk.NS)
-    splineList.bind('<<ListboxSelect>>', ListChanged)
-    splineList.insert(tk.END, Spline([1,2,3]))
-    splineList.insert(tk.END, Spline([-1,-2,-3]))
-    for i in range(30):
-        splineList.insert(tk.END, Spline([i,0,-1]))
-    verticalScroll = tk.Scrollbar(root, orient=tk.VERTICAL)
-    verticalScroll.grid(row=0, column=1, sticky=tk.NS)
-    splineList.configure(yscrollcommand=verticalScroll.set)
-    verticalScroll.config(command=splineList.yview)
-    frame = SplineOpenGLFrame(root, width=500, height=500)
-    frame.grid(row=0, column=2)
-    root.mainloop()
+    app = PyNubApp()
+    for i in range(16):
+        app.AddSpline(Spline([[-1, i/16.0], [0, -i/16.0], [1,0]]))
+    app.mainloop()
