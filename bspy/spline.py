f797f9c19af235f797181fd87d192c6ff450ca11
Eric Brechner <ericbrec@msn.com>
2021-12-31 13:07:21 -0800

    Default to shaded with boundary.

--- bspy/spline.py
+++ bspy/spline.py
@@ -27,7 +27,7 @@ class Spline:
         self.coefficients = coefficients
         self.fillColor = np.array((0.0, 1.0, 0.0, 1.0), np.float32)
         self.lineColor = np.array((1.0, 1.0, 1.0, 1.0), np.float32)
-        self.options = self.SHADED
+        self.options = self.SHADED | self.BOUNDARY
 
     def __str__(self):
         return "[{0}, {1}]".format(self.coefficients[0], self.coefficients[1])
