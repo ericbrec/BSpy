Minor fixes for evaluate, derivative, and extrapolate.

bspy/spline.py
@@ -405,7 +405,7 @@ class Spline:
             ix = min(ix, self.nCoef[iv])
             myIndices.append(ix)
             mySection.append(slice(ix - self.order[iv], ix))
-        myCoefs = self.coefs[mySection]
+        myCoefs = self.coefs[tuple(mySection)]
         for iv in range(self.nInd - 1, -1, -1):
             bValues = b_spline_values(myIndices[iv], self.knots[iv], self.order[iv], with_respect_to[iv], uvw[iv])
             myCoefs = myCoefs @ bValues
@@ -689,7 +689,7 @@ class Spline:
             ix = min(ix, self.nCoef[iv])
             myIndices.append(ix)
             mySection.append(slice(ix - self.order[iv], ix))
-        myCoefs = self.coefs[mySection]
+        myCoefs = self.coefs[tuple(mySection)]
         for iv in range(self.nInd - 1, -1, -1):
             bValues = b_spline_values(myIndices[iv], self.knots[iv], self.order[iv], uvw[iv])
             myCoefs = myCoefs @ bValues
@@ -783,7 +783,7 @@ class Spline:
             # Store slicer for current independent variable to restore later.
             slicer = sliceJI[ind + 2]
 
-            # Compute derivatives of coefficients at interior knots.
+            # Compute adjusted derivatives of coefficients at interior knots (see similar code for elevate_and_insert_knots).
             for j in range(1, continuity + 1):
                 sliceJI[0] = j
                 sliceJm1Ip1[0] = j - 1
@@ -793,10 +793,10 @@ class Spline:
                     sliceJm1Ip1[ind + 2] = i + 1
                     sliceJm1I[ind + 2] = i
                     gap = knots[ind][i + self.order[ind]] - knots[ind][i + j]
-                    alpha = (self.order[ind] - j) / gap if gap > 0.0 else 0.0
+                    alpha = 1.0 / gap if gap > 0.0 else 0.0
                     dCoefs[tuple(sliceJI)] = alpha * (dCoefs[tuple(sliceJm1Ip1)] - dCoefs[tuple(sliceJm1I)])
             
-            # Extrapolate left side as needed by integrating coefficients to new exterior knots.
+            # Extrapolate left side as needed by integrating coefficients to new exterior knots (see similar code for elevate_and_insert_knots).
             if bounds[0] is not None and not np.isnan(bounds[0]):
                 for j in range(continuity, 0, -1):
                     sliceJm1Ip1[0] = j - 1
@@ -818,7 +818,7 @@ class Spline:
                     sliceJm1Ip1[ind + 2] = slicer.stop - 1
                     sliceJm1I[ind + 2] = slicer.stop - 1 - j
                     dCoefs[tuple(sliceJm1Ip1)] = dCoefs[tuple(sliceJm1I)]
-                # Now we can integrate the coefficients as usual.
+                # Now we can integrate the coefficients as usual (see similar code for elevate_and_insert_knots).
                 for j in range(continuity, 0, -1):
                     sliceJm1Ip1[0] = j - 1
                     sliceJm1I[0] = j - 1

tests/test_bspy.py
@@ -636,12 +636,12 @@ def test_evaluate():
             i += 1
     assert maxerror <= 2.5 * np.finfo(float).eps
 
-def _test_extrapolate():
+def test_extrapolate():
     maxerror = 0.0
-    #spline = bspy.Spline(1, 2, (4,), (6,), [np.array([0, 0, 0, 0.2, 0.3, 0.4, 0.5, 0.5, 1, 1], float)], 
-    #    np.array(((260, 100), (100, 260), (260, 420), (420, 420), (580, 260), (420, 100)), float))
-    spline = bspy.Spline(1, 2, (4,), (5,), [np.array([0, 0, 0.2, 0.2, 0.5, 0.8, 0.8, 1, 1], float)], 
-        np.array(((260, 100), (100, 260), (300, 300), (100, 260), (260, 100)), float))
+    spline = bspy.Spline(1, 2, (4,), (6,), [np.array([0, 0, 0, 0.2, 0.3, 0.4, 0.5, 0.5, 1, 1], float)], 
+        np.array(((260, 100), (100, 260), (260, 420), (420, 420), (580, 260), (420, 100)), float)).clamp((0,), (0,))
+    #spline = bspy.Spline(1, 2, (4,), (5,), [np.array([0, 0, 0.2, 0.2, 0.5, 0.8, 0.8, 1, 1], float)], 
+    #    np.array(((260, 100), (100, 260), (300, 300), (100, 260), (260, 100)), float))
     extrapolated = spline.extrapolate([[-0.5, 1.5]], [2])
     maxerror = 0.0
     for u in np.linspace(spline.knots[0][spline.order[0]-1], spline.knots[0][spline.nCoef[0]], 3):