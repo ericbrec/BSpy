ec409e69929cfa088741b9ee303c51063fd0e0b6
Tom <thomas@grandine.org>
2023-06-26 12:13:03 -0700

    New zeros test case for interval results

--- tests/test_bspy.py
+++ tests/test_bspy.py
@@ -1002,6 +1002,10 @@ def test_zeros():
     roots = spline.zeros(tolerance)
     check_1D_roots(expectedRoots, roots, tolerance)
 
+    spline = bspy.Spline(1, 1, (4,), (6,), ((0.0, 0.0, 0.0, 0.0, 0.3, 0.7, 1.0, 1.0, 1.0, 1.0),), ((1.3, 0, 0, 0, 0, -2.6),))
+    roots = spline.zeros()
+    # TODO: Make this test case work
+
     data = []
     for u in np.linspace(-2.0, 2.0, 3):
         for v in np.linspace(-2.0, 2.0, 7):
