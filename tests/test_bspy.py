6658b7b0df9744bfb44376cfa1938b384d09f9e5
Tom <thomas@grandine.org>
2023-07-08 15:39:57 -0700

    Eliminate unneeded tolerance arguments

--- tests/test_bspy.py
+++ tests/test_bspy.py
@@ -983,27 +983,27 @@ def test_zeros():
 
     spline = bspy.Spline(1, 1, (4,), (5,), ((0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 2.0, 2.0, 2.0),), ((1.0, -2.0, -2.0, 3.0, -1.0),))
     expectedRoots = (0.12679064971673917, 1.1797004713504056, 1.9015253538967793)
-    roots = spline.zeros(tolerance)
+    roots = spline.zeros()
     check_1D_roots(expectedRoots, roots, tolerance)
 
     spline = bspy.Spline(1, 1, (4,), (10,), ((0.0, 0.0, 0.0, 0.0, 1.0, 3.0, 4.0, 7.0, 7.0, 8.0, 10.0, 10.0, 10.0, 10.0),), ((1.0, -2.0, -3.0, -4.0, 5.0, 6.0, 7.0, -8.0, -9.0, 1.0),))
     expectedRoots = (0.1242770027023396, 3.556169240708571, 7.866681344525834, 9.930791005970617)
-    roots = spline.zeros(tolerance)
+    roots = spline.zeros()
     check_1D_roots(expectedRoots, roots, tolerance)
 
     spline = bspy.Spline(1, 1, (5,), (7,), ((0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 2.0, 4.0, 4.0, 4.0, 4.0, 4.0),), ((1.0, -2.0, -3.0, 4.0, 5.0, -6.0, 1.0),))
     expectedRoots = (0.09400220779701107, 1.2018213294830917, 3.019267586117955, 3.918820194105917)
-    roots = spline.zeros(tolerance)
+    roots = spline.zeros()
     check_1D_roots(expectedRoots, roots, tolerance)
 
     spline = bspy.Spline(1, 1, (6,), (11,), ((0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 2.0, 2.0, 2.0, 4.0, 4.0, 4.0, 4.0, 4.0, 4.0),), ((2.0, -1.0, -1.0, -1.0, 1.0, 2.0, 2.0, -3.0, -3.0, 4.0, 1.0),))
     expectedRoots = (0.19780681921299614, 0.959258605483323, 2.3682761525810267, 3.3362282254078597)
-    roots = spline.zeros(tolerance)
+    roots = spline.zeros()
     check_1D_roots(expectedRoots, roots, tolerance)
 
     spline = bspy.Spline(1, 1, (4,), (4,), ((0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0),), ((1.0, -13.0 / 9.0, 25.0 / 12.0, -3.0),))
     expectedRoots = (0.39999999558761995, 0.4285714285714262)
-    roots = spline.zeros(tolerance)
+    roots = spline.zeros()
     check_1D_roots(expectedRoots, roots, tolerance)
 
     spline = bspy.Spline(1, 1, (4,), (6,), ((0.0, 0.0, 0.0, 0.0, 0.3, 0.7, 1.0, 1.0, 1.0, 1.0),), ((1.3, 0, 0, 0, 0, -2.6),))
