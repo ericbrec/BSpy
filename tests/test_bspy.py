6fe3010e71512ca6c3f671696c652e56b5d650e5
Eric Brechner <ericbrec@msn.com>
2023-08-27 09:37:28 -0700

    Fixed intersect test

--- tests/test_bspy.py
+++ tests/test_bspy.py
@@ -842,6 +842,11 @@ def test_intersect():
 
     return # Comment this line to run the following additional really lengthy test
 
+    maxError = 0.0
+    F = lambda u , v : (u ** 2 + (v - 3/4) ** 2 - 1/25) * \
+        ((u - 2/5) ** 2 + (v - 3/5) ** 2 - 1/25) * \
+        (u ** 2 + (v - 3/2) ** 2 - 25/16) * \
+        ((u - 1) ** 2 + (v - 3/10) ** 2 - 1/25)
     order = 9
     knots = [0.0] * order + [1.0] * order
     nCoef = order
