96a6ea149b471440d6b43f7b38a3415bde50f393
Tom <thomas@grandine.org>
2023-12-30 09:49:49 -0800

    Adjust examples to accommodate graphics changes

--- examples/primitives.py
+++ examples/primitives.py
@@ -14,9 +14,28 @@ crv4 = [[0, 0], [1, 0], [0, 1]] @ bspy.Spline.section([[0.0, 0.0, 90.0, -0.7], [
 patch40 = bspy.Spline.four_sided_patch(crv1, crv2, crv3, crv4, 0.0)
 patch405 = bspy.Spline.four_sided_patch(crv1, crv2, crv3, crv4, 0.5)
 patch41 = bspy.Spline.four_sided_patch(crv1, crv2, crv3, crv4, 1.0)
-patch40.metadata = dict(Name = 'patch40')
-patch405.metadata = dict(Name = 'patch405')
-patch41.metadata = dict(Name = 'patch41')
+patch40.set_fill_color(0.0, 0.8, 0.2, 0.6)
+patch405.set_fill_color(0.0, 0.6, 0.4, 0.8)
+patch41.set_fill_color(0.0, 0.4, 0.6, 1.0)
+bottomSurf = bspy.Spline(2, 3, [4, 4], [4, 4], 2 * [[0.0, 0, 0, 0, 1, 1, 1, 1]],
+                                         [4 * [0.0, 0.3, 0.7, 1],
+                                          [0.0, 0, 0, 0, 0.3, 0.3, 0.3, 0.3,
+                                           0.7, 0.7, 0.7, 0.7, 1, 1, 1, 1],
+                                          [0.0, 0.0, 0.2, 0.0, 0.0, 0.8, 0.0, 0.0,
+                                           0.2, 0.3, 0.7, 0.2, 0.0, 0.6, 0.0, 0.0]])
+topSurf = bottomSurf + [0, 0, 1]
+mySolid = bspy.Spline.ruled_surface(bottomSurf, topSurf)
+
+if __name__=='__main__':
+    app = bspy.bspyApp()
+    app.list(mySphere, 'mySphere')
+    app.list(myTorus, 'myTorus')
+    app.list(patch40, 'patch40')
+    app.list(patch405, 'patch405')
+    app.list(patch41, 'patch41')
+    app.list(mySolid, 'mySolid')
+    app.mainloop()
+
 
 if __name__=='__main__':
     app = bspy.bspyApp()
