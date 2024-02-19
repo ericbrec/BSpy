1d1276ef67a229dee359fb3f3c744487f10fd51c
Eric Brechner <ericbrec@msn.com>
2024-02-18 17:25:00 -0800

    Add extended tests and remove blossom

--- bspy/_spline_evaluation.py
+++ bspy/_spline_evaluation.py
@@ -1,41 +1,5 @@
 import numpy as np
 
-def blossom(self, uvw):
-    def blossom_values(knot, knots, order, u):
-        basis = np.zeros(order, knots.dtype)
-        basis[-1] = 1.0
-        for degree in range(1, order):
-            b = order - degree
-            for i in range(knot - degree, knot):
-                alpha = (u[degree - 1] - knots[i]) / (knots[i + degree] - knots[i])
-                basis[b - 1] += (1.0 - alpha) * basis[b]
-                basis[b] *= alpha
-                b += 1
-        return basis
-
-    # Make work for scalar valued functions
-    uvw = np.atleast_1d(uvw)
-
-    # Check for evaluation point inside domain
-    dom = self.domain()
-    for ix in range(self.nInd):
-        if uvw[ix][0] < dom[ix][0] or uvw[ix][self.order[ix]-2] > dom[ix][1]:
-            raise ValueError(f"Spline evaluation outside domain: {uvw}")
-
-    # Grab all of the appropriate coefficients
-    mySection = [slice(0, self.nDep)]
-    myIndices = []
-    for iv in range(self.nInd):
-        ix = np.searchsorted(self.knots[iv], uvw[iv][0], 'right')
-        ix = min(ix, self.nCoef[iv])
-        myIndices.append(ix)
-        mySection.append(slice(ix - self.order[iv], ix))
-    myCoefs = self.coefs[tuple(mySection)]
-    for iv in range(self.nInd - 1, -1, -1):
-        bValues = blossom_values(myIndices[iv], self.knots[iv], self.order[iv], uvw[iv])
-        myCoefs = myCoefs @ bValues
-    return myCoefs
-
 def bspline_values(knot, knots, splineOrder, u, derivativeOrder = 0, taylorCoefs = False):
     basis = np.zeros(splineOrder, knots.dtype)
     basis[-1] = 1.0
