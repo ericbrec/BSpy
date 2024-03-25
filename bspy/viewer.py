c292fb2a89df8ab4560d78f67b4a35333b4d8e0b
Eric Brechner <ericbrec@msn.com>
2024-03-25 12:04:02 -0700

    Fix selection for solids

--- bspy/viewer.py
+++ bspy/viewer.py
@@ -195,13 +195,13 @@ class Viewer(tk.Tk):
     def list_solid(self, solid, name="Solid", fillColor=None, lineColor=None, options=None, inherit=True, draw=False):
         solid.isDrawn = draw
         solid.isSelected = draw
-        iid = self.treeview.insert('', 'end', text=name, open=True)
+        iid = self.treeview.insert('', 'end', text=name, open=False)
         self.splineList[iid] = solid
         self.solidList.append(solid)
         if draw:
             self.treeview.selection_add(iid)
         for i, surface in enumerate(solid.boundaries):
-            self.list_boundary(surface, f"{name} boundary {i+1}", fillColor, lineColor, options, inherit, draw, iid)
+            self.list_boundary(surface, f"{name} boundary {i+1}", fillColor, lineColor, options, inherit, False, iid)
 
     def draw_solid(self, solid, name="Solid", fillColor=None, lineColor=None, options=None, inherit=True):
         self.list_solid(solid, name, fillColor, lineColor, options, inherit, draw=True)
@@ -269,9 +269,6 @@ class Viewer(tk.Tk):
     def update(self):
         """Update the spline draw list, set the default view, reset the bounds, and refresh the frame."""
         self.splineDrawList = []
-        for solid in self.solidList:
-            solid.isSelected = False
-
         gotOne = False
         for item in self.treeview.selection():
             spline = self.splineList[item]
@@ -287,14 +284,18 @@ class Viewer(tk.Tk):
                     gotOne = True
                 self.splineDrawList.append(spline)
             elif isinstance(spline, Solid):
-                spline.isSelected = True
-                if not spline.isDrawn:
-                    spline.isDrawn = True
-                    self.treeview.selection_add(self.treeview.get_children(item))
-
-        for solid in self.solidList:
-            if not solid.isSelected:
-                solid.isDrawn = False
+                for subitem in self.treeview.get_children(item):
+                    spline = self.splineList[subitem]
+                    coefs = spline.cache["coefs32"].T[:3]
+                    coefsAxis = tuple(range(1, spline.nInd + 1))
+                    if gotOne:
+                        splineMin = np.minimum(splineMin, coefs.min(axis=coefsAxis))
+                        splineMax = np.maximum(splineMax, coefs.max(axis=coefsAxis))
+                    else:
+                        splineMin = coefs.min(axis=coefsAxis)
+                        splineMax = coefs.max(axis=coefsAxis)
+                        gotOne = True
+                    self.splineDrawList.append(spline)
 
         if gotOne:
             newRadius = 0.5 * np.max(splineMax - splineMin)
