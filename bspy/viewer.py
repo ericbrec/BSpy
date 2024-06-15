46f272266d555b6a4d2c06e6524f48d6a56496f8
Eric Brechner <ericbrec@msn.com>
2024-06-15 14:43:04 -0700

    Minor fixes to Viewer and Hyperplane

--- bspy/viewer.py
+++ bspy/viewer.py
@@ -212,7 +212,7 @@ class Viewer(tk.Tk):
                 if isinstance(spline, Spline):
                     self.list(spline)
                 else:
-                    self.list_solid(spline)
+                    self.list(spline)
     
     def erase_all(self):
         """Stop drawing all splines. Splines remain in the treeview."""
