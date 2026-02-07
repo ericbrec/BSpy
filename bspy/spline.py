69d39ee434eba08c93ed856175789bfce6f0c94e
Eric Brechner <ericbrec@msn.com>
2026-02-06 21:14:57 -0800

    Fix Spline.intersect documentation

--- bspy/spline.py
+++ bspy/spline.py
@@ -1324,18 +1324,18 @@ class Spline(Manifold):
             A list of intersections between the two manifolds. 
             Each intersection records either a crossing or a coincident region.
 
-            For a crossing, intersection is a `Manifold.Crossing`: (left, right)
-            * left : `Manifold` in the manifold's domain where the manifold and the other cross.
-            * right : `Manifold` in the other's domain where the manifold and the other cross.
+            For a crossing, intersection is a Manifold.Crossing: (firstPart, secondPart)
+            * firstPart : `Manifold` in the manifold's domain where the manifold and the other cross.
+            * secondPart : `Manifold` in the other's domain where the manifold and the other cross.
             * Both intersection manifolds have the same domain and range (the crossing between the manifold and the other).
 
-            For a coincident region, intersection is a `Manifold.Coincidence`: (left, right, alignment, transform, inverse, translation)
-            * left : `Solid` in the manifold's domain within which the manifold and the other are coincident.
-            * right : `Solid` in the other's domain within which the manifold and the other are coincident.
+            For a coincident region, intersection is Manifold.Coincidence: (firstPart, secondPart, alignment, transform, inverse, translation)
+            * firstPart : `Solid` in the manifold's domain within which the manifold and the other are coincident.
+            * secondPart : `Solid` in the other's domain within which the manifold and the other are coincident.
             * alignment : scalar value holding the normal alignment between the manifold and the other (the dot product of their unit normals).
-            * transform : `numpy.array` holding the transform matrix from the manifold's domain to the other's domain.
-            * inverse : `numpy.array` holding the inverse transform matrix from the other's domain to the boundary's domain.
-            * translation : `numpy.array` holding the translation vector from the manifold's domain to the other's domain.
+            * transform : `numpy.array` holding the matrix transform from the boundary's domain to the other's domain.
+            * inverse : `numpy.array` holding the matrix inverse transform from the other's domain to the boundary's domain.
+            * translation : `numpy.array` holding the 1D translation from the manifold's domain to the other's domain.
             * Together transform, inverse, and translation form the mapping from the manifold's domain to the other's domain and vice-versa.
         
         See Also
